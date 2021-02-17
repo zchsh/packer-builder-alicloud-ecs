52fe99dd320048436c0ede4fa37687c9e2639fd7
Zach Shilton <4624598+zchsh@users.noreply.github.com>
2021-02-17 11:44:58 -0500

    Set up repo

---
+++ refresh-content.sh
@@ -0,0 +1,4 @@
+rm -rf some-content-folder
+mkdir some-content-folder
+mkdir some-content-folder/content-files
+cp ../packer/website/content/docs/builders/alicloud-ecs.mdx ./some-content-folder/content-files/alicloud-ecs.mdx
